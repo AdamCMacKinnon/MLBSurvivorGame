9fd9bce43469267e55c5df6d6c6c00ff8fbcebe6
Adam MacKinnon <mackinnon.adam@gmail.com>
2022-04-30 14:37:29 -0400

    clean up code, but rendering picks now

--- routes/gamepage.js
+++ routes/gamepage.js
@@ -7,12 +7,26 @@ const { sequelize } = require('../models');
 const { TEXT } = require('sequelize');
 
 router.get('/gamepage', async (req,res) => {
-  const seePicks = comparePicks(userid, userpick)
   let userid = req.session.id
   let isactive = req.session.isactive
   let user = req.session.username
+  const picksArr = 
+  await models.picks.findAll({
+    where: {
+      userid: userid
+    },
+    attributes: ['picks'],
+    raw: true
+  })
+  const seePicks = JSON.stringify(picksArr)
+  let result = picksArr.map(p => p.picks)
+  const picksResult = result[0]
+  console.log("PICKS RESULT " + picksResult)
+  console.log(seePicks)
+  console.log("RESULT " + result)
   if (isactive === true) {
-    res.render('gamepage', { alert: `Hello ${user}, You are currently ACTIVE`})
+    res.render('gamepage', { alert: `Hello ${user}, You are currently ACTIVE`} &&
+    { picksResult: result })
     } else {
       res.render('gamepage', { alert: `Hello ${user}, you have been ELIMINATED`})
     }
@@ -23,7 +37,6 @@ router.post('/gamepage', async (req,res) => {
   const status = req.session.isactive
   const userid = req.session.id
   const userpick = [req.body.pick]
-
   if (status === false) {
     res.render('gamepage', { message: 'Sorry, you have been eliminated!' })
   } else {
@@ -33,12 +46,6 @@ router.post('/gamepage', async (req,res) => {
         }
       })
       if (findId !== null) {
-        const seePicks = await comparePicks(userpick, userid)
-        if (seePicks == true) {
-          res.render('gamepage', { message: `You already picked the ${userpick}` })
-        } else {
-          res.render('gamepage', {message: `Current Pick: ${userpick}`})
-        }
         await models.picks.update({
           'picks': sequelize.fn('array_append', sequelize.col('picks', TEXT), userpick.toString())
         }, {
@@ -78,13 +85,11 @@ async function comparePicks(userpick, userid) {
     attributes: ['picks'],
     raw: true
   })
-  console.log(picksArr)
-  for (let p = 1; p < picksArr.length; p++) {
-    if (picksArr.picks[p] === userpick) {
+  console.log("OBJECT " + Object.values(picksArr[0]))
+  for (let p = 0; p < picksArr.length; p++) {
+    if (Object.values(picksArr)[p] === userpick) {
       return true
-    } else {
-      return false
-    }
+    } 
   }
 }
 
